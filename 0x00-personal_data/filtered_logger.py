2794be7fa97552692e94d79080433fa32e6b0841
mire-web <amahe8664@gmail.com>
2024-09-12 22:22:23 +0100

    log

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,8 +8,7 @@ import re
 def filter_datum(fields: list[str], redaction: str,
                  message: str, separator: str) -> str:
     """Filter Information"""
-    msg = message
     for field in fields:
-        msg = re.sub(r'{}=(.*?){}'.format(field, separator),
-                     '{}={}{}'.format(field, redaction, separator), msg)
+        message = re.sub(r'{}=(.*?){}'.format(field, separator),
+                         '{}={}{}'.format(field, redaction, separator), message)
     return msg
