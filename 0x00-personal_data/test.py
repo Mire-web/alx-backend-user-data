f2897ce71886ba49eee8dddaaa7b3101a6a3b3de
mire-web <amahe8664@gmail.com>
2024-09-13 13:53:34 +0100

    filter_log

--- 0x00-personal_data/test.py
+++ 0x00-personal_data/test.py
@@ -3,7 +3,7 @@
 Main file
 """
 
-filter_datum = __import__('filtered_logger').filter_datum
+filter_datum = __import__('filter_logger').filter_datum
 
 fields = ["password", "date_of_birth"]
 messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
